023c38c74d9fd058671b0bea3c9cc7dade9ec3a2
Ambrose <esorbma.nosilla@icloud.com>
2024-11-20 14:20:19 -0800

    Map code updates
    - Started adding functionality to update the map image when navigating to different buildings
    - Added custom marker icons
    - Removed clicking to add markers

--- index/main.js
+++ index/main.js
@@ -8,18 +8,27 @@ function dropdown() {
 }
 
 
+window.addEventListener("hashchange", (e) => {
+  loadMap(window.location.hash.substring(1))
+})
+
 var map = L.map('map', {
-    crs: L.CRS.Simple,
-    minZoom: -5
+  crs: L.CRS.Simple,
+  minZoom: -5
 });
 var bounds = [[0,0], [2200,3400]]
-var image = L.imageOverlay('../floor_plans/SW01_F3.png', bounds).addTo(map)
-map.fitBounds(bounds)
 
-var marker = L.marker([900,2200]).bindPopup('Ethernet Port:<br>3350 A').addTo(map)
-
-function onMapClick(e) {
-    L.marker([e.latlng.lat, e.latlng.lng]).addTo(map)
+function loadMap(mapLocation) {
+  var image = L.imageOverlay(`../floor_plans/${mapLocation}.png`, bounds).addTo(map)
+  map.fitBounds(bounds)
 }
 
-map.on('click', onMapClick);
+var iconConfig = L.icon({
+  iconUrl: 'icons/ethernet_port.png',
+  iconSize: [32, 32],
+  iconAnchor: [16, 32],
+  popupAnchor: [0, -32]
+})
+
+// var marker = L.marker([900,2200], {icon: iconConfig}).bindPopup('Ethernet Port:<br>3350 A').addTo(map)
+
